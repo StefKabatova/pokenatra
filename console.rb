8bcf10d6099ea8f40e865e5a75fde8f29c8bf356
StefKabatova <stefkabatova@gmail.com>
2016-11-01 15:55:44 -0400

    mvp version

---
+++ console.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+require 'pg'
+require 'active_record'
+require 'pry'
+
+require_relative 'connection'
+require_relative '../models/pokemon'
+
+
+
+
+
+binding.pry
+
+puts "this line fixes binding.pry bug"
